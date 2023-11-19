8ea6f11a5b8082bfb7fae5c5820476a1a339d5f1
iusmar.lng@gmail.com <iusmar.lng@gmail.com>
2023-11-19 15:49:23 +0100

    added the 2nd email for receiving notifications

--- SendGridMailSender/Services/EmailSender.cs
+++ SendGridMailSender/Services/EmailSender.cs
@@ -8,14 +8,17 @@ namespace SendGridMailSender.Services
     {
         private readonly string _apiKey = "";
         private readonly string _adminFromEmail = "";
-        private readonly string _notificationEmail = "";
+        private readonly string _notificationEmail1 = "";
+        private readonly string _notificationEmail2 = "";
         private readonly ILogger<EmailSender> _logger;
 
         public EmailSender(ILogger<EmailSender> logger)
         {
             _apiKey = Environment.GetEnvironmentVariable("sendgridApiKey");
             _adminFromEmail = Environment.GetEnvironmentVariable("adminFromEmail");
-            _notificationEmail = Environment.GetEnvironmentVariable("notificationEmail1");
+            _notificationEmail1 = Environment.GetEnvironmentVariable("notificationEmail1");
+            _notificationEmail2 = Environment.GetEnvironmentVariable("notificationEmail2");
+
             _logger = logger;
         }
         public async Task SendContactUsEmail(string userEmail, string userName, string body)
@@ -24,11 +27,11 @@ namespace SendGridMailSender.Services
             {
                 var client = new SendGridClient(_apiKey);
                 var from = new EmailAddress(_adminFromEmail);
-                var to = new EmailAddress(_notificationEmail);
+                var to = new List<EmailAddress>() {new EmailAddress(_notificationEmail1),new EmailAddress(_notificationEmail2)};
                 var subject = $"Contact Us Request from {userName}";
                 var plainTextContent = $"{userEmail} said:\n{body}";
                 var htmlContent = $"{userEmail} said <p>{body}</p>";
-                var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+                var msg = MailHelper.CreateSingleEmailToMultipleRecipients(from, to, subject, plainTextContent, htmlContent);
 
                 await client.SendEmailAsync(msg);
             }
@@ -45,10 +48,10 @@ namespace SendGridMailSender.Services
                 var client = new SendGridClient(_apiKey);
                 var from = new EmailAddress(_adminFromEmail);
                 var subject = "New File Upload Notification";
-                var to = new EmailAddress(_notificationEmail);
+                var to = new List<EmailAddress>() { new EmailAddress(_notificationEmail1), new EmailAddress(_notificationEmail2) };
                 var plainTextContent = $"Filename {filename} has been uploaded.";
                 var htmlContent = $"Filename: <strong>{filename}</strong> has been uploaded.";
-                var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+                var msg = MailHelper.CreateSingleEmailToMultipleRecipients(from, to, subject, plainTextContent, htmlContent);
 
                 await client.SendEmailAsync(msg);
             }
