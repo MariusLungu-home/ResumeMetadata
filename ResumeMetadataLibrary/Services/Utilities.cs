88f4805e8c4fc1881a0da560efd2c42351114207
iusmar.lng@gmail.com <iusmar.lng@gmail.com>
2023-11-20 17:29:36 +0100

    added logs in the Utilities.cs class

--- ResumeMetadataLibrary/Services/Utilities.cs
+++ ResumeMetadataLibrary/Services/Utilities.cs
@@ -3,15 +3,21 @@ using DocumentFormat.OpenXml.Wordprocessing;
 using PdfSharp.Pdf;
 using PdfSharp.Drawing;
 using PdfSharp.Pdf.IO;
-using ResumeMetadataLibrary.Services;
 using PdfSharp.Fonts;
+using ResumeMetadataLibrary.Services;
 using ResumeMetadataLibrary.Resolvers;
+using Microsoft.Extensions.Logging;
 
 public class Utilities : IUtilities
 {
 
     private static bool _fontResolverSet = false;
+    private readonly ILogger<Utilities> _logger;
 
+    public Utilities(ILogger<Utilities> logger)
+    {
+        _logger = logger;
+    }
     public static void InitializeFontResolver()
     {
         if (!_fontResolverSet)
@@ -23,112 +29,127 @@ public class Utilities : IUtilities
 
     public Task<Stream> InsertMetadataDocx(Stream source, string metaDataToInsert)
     {
-        // Open the Word document
-        using (WordprocessingDocument wordDocument = WordprocessingDocument.Open(source, true))
+        try
         {
+            // Open the Word document
+            using (WordprocessingDocument wordDocument = WordprocessingDocument.Open(source, true))
+            {
 
-            // Get the main document part
-            MainDocumentPart mainPart = wordDocument.MainDocumentPart 
-                ?? throw new ArgumentNullException("The document has no body.");
+                // Get the main document part
+                MainDocumentPart mainPart = wordDocument.MainDocumentPart
+                    ?? throw new ArgumentNullException("The document has no body.");
 
-            // Create a new table with one row and two columns
-            Table table = new Table(
-                        new TableProperties(
-                            new TableBorders(
-                                new InsideHorizontalBorder(),
-                                new InsideVerticalBorder()
-                            )
-                        ),
-                        new TableRow(
-                            new TableCell(
-                                new TableCellProperties(
-                                    new TableCellWidth { Type = TableWidthUnitValues.Dxa, Width = "1440" } // 1 cm
-                                ),
-                                new Paragraph(
-                                    new Run(
-                                        new RunProperties(
-                                            new FontSize { Val = "1" },
-                                            new Color { Val = "FFFFFF" }
-                                        ),
-                                        new Text("Résumé Metadata")
-                                    )
+                // Create a new table with one row and two columns
+                Table table = new Table(
+                            new TableProperties(
+                                new TableBorders(
+                                    new InsideHorizontalBorder(),
+                                    new InsideVerticalBorder()
                                 )
                             ),
-                            new TableCell(
-                                new TableCellProperties(
-                                    new TableCellWidth { Type = TableWidthUnitValues.Dxa, Width = "28800" } // 20 cm
+                            new TableRow(
+                                new TableCell(
+                                    new TableCellProperties(
+                                        new TableCellWidth { Type = TableWidthUnitValues.Dxa, Width = "1440" } // 1 cm
+                                    ),
+                                    new Paragraph(
+                                        new Run(
+                                            new RunProperties(
+                                                new FontSize { Val = "1" },
+                                                new Color { Val = "FFFFFF" }
+                                            ),
+                                            new Text("Résumé Metadata")
+                                        )
+                                    )
                                 ),
-                                new Paragraph(
-                                    new Run(
-                                        new RunProperties(
-                                            new FontSize { Val = "1" },
-                                            new Color { Val = "FFFFFF" }
-                                        ),
-                                        new Text(metaDataToInsert)
+                                new TableCell(
+                                    new TableCellProperties(
+                                        new TableCellWidth { Type = TableWidthUnitValues.Dxa, Width = "28800" } // 20 cm
+                                    ),
+                                    new Paragraph(
+                                        new Run(
+                                            new RunProperties(
+                                                new FontSize { Val = "1" },
+                                                new Color { Val = "FFFFFF" }
+                                            ),
+                                            new Text(metaDataToInsert)
+                                        )
                                     )
                                 )
-                            )
-                        ));
+                            ));
 
-            // Append the table to the document body
-            mainPart.Document.Body?.AppendChild(table);
+                // Append the table to the document body
+                mainPart.Document.Body?.AppendChild(table);
 
-            // Iterate through the table cells and set spacing after to zero for paragraphs
-            foreach (var row in table.Elements<TableRow>())
-            {
-                foreach (var cell in row.Elements<TableCell>())
+                // Iterate through the table cells and set spacing after to zero for paragraphs
+                foreach (var row in table.Elements<TableRow>())
                 {
-                    foreach (var paragraph in cell.Elements<Paragraph>())
+                    foreach (var cell in row.Elements<TableCell>())
                     {
-                        var paragraphProperties = paragraph.Elements<ParagraphProperties>().FirstOrDefault();
-                        if (paragraphProperties == null)
+                        foreach (var paragraph in cell.Elements<Paragraph>())
                         {
-                            paragraphProperties = new ParagraphProperties();
-                            paragraph.PrependChild(paragraphProperties); // Prepend to add before the run
-                        }
+                            var paragraphProperties = paragraph.Elements<ParagraphProperties>().FirstOrDefault();
+                            if (paragraphProperties == null)
+                            {
+                                paragraphProperties = new ParagraphProperties();
+                                paragraph.PrependChild(paragraphProperties); // Prepend to add before the run
+                            }
 
-                        var spacingBetweenLines = paragraphProperties.Elements<SpacingBetweenLines>().FirstOrDefault();
-                        if (spacingBetweenLines == null)
-                        {
-                            spacingBetweenLines = new SpacingBetweenLines { After = "0" };
-                            paragraphProperties.Append(spacingBetweenLines);
-                        }
-                        else
-                        {
-                            spacingBetweenLines.After = "0";
+                            var spacingBetweenLines = paragraphProperties.Elements<SpacingBetweenLines>().FirstOrDefault();
+                            if (spacingBetweenLines == null)
+                            {
+                                spacingBetweenLines = new SpacingBetweenLines { After = "0" };
+                                paragraphProperties.Append(spacingBetweenLines);
+                            }
+                            else
+                            {
+                                spacingBetweenLines.After = "0";
+                            }
                         }
                     }
                 }
+
+                // Find the paragraph immediately before the table and remove it
+                var carriageReturnPrecedingTable = table.Parent.ChildElements[table.Parent.ChildElements.Count - 3];
+                if (carriageReturnPrecedingTable.LocalName == "p")
+                    table.Parent.ChildElements[table.Parent.ChildElements.Count - 3].Remove();
+
+                // Save the document
+                mainPart.Document.Save();
             }
 
-            // Find the paragraph immediately before the table and remove it
-            var carriageReturnPrecedingTable = table.Parent.ChildElements[table.Parent.ChildElements.Count - 3];
-            if (carriageReturnPrecedingTable.LocalName == "p")
-                table.Parent.ChildElements[table.Parent.ChildElements.Count - 3].Remove();
-
-            // Save the document
-            mainPart.Document.Save();
+            return Task.Run(() => source);
         }
-
-        return Task.Run(() => source);
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"ERROR injecting metadata into DOCX file. {ex.Message}");
+            return Task.Run(() => source);
+        }
     }
 
     public Task<Stream> InsertMetadataPdf(Stream source, string metaDataToInsert)
     {
-        InitializeFontResolver();
+        try
+        {
+            InitializeFontResolver();
 
-        PdfDocument document = PdfReader.Open(source, PdfDocumentOpenMode.Modify);
-        PdfPage page = document.Pages[0];
-        XGraphics gfx = XGraphics.FromPdfPage(page);
-        XFont font = new XFont("Arial", 0.01, XFontStyleEx.Regular);
-        XBrush brush = new XSolidBrush(XColor.FromArgb(1, 1, 1, 1)); // Transparent brush
+            PdfDocument document = PdfReader.Open(source, PdfDocumentOpenMode.Modify);
+            PdfPage page = document.Pages[0];
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+            XFont font = new XFont("Arial", 0.01, XFontStyleEx.Regular);
+            XBrush brush = new XSolidBrush(XColor.FromArgb(1, 1, 1, 1)); // Transparent brush
 
-        gfx.DrawString(metaDataToInsert, font, brush, new XRect(0, 0, page.Width, page.Height), XStringFormats.BottomRight);
+            gfx.DrawString(metaDataToInsert, font, brush, new XRect(0, 0, page.Width, page.Height), XStringFormats.BottomRight);
 
+            document.Save(source);
+            document.Close();
 
-        document.Save(source);
-        document.Close();
-
-        return Task.Run(() => source);
+            return Task.Run(() => source);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"ERROR injecting metadata into PDF file. {ex.Message}");
+            return Task.Run(() => source);
+        }
     }
 }
